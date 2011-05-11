42cf2fd154d5a3f12abb284f27a5fdaaa183ecf9
Nathan Bashaw <nbashaw@gmail.com>
2011-05-10 21:54:37 -0400

    it's alive

--- sourcebook.rb
+++ sourcebook.rb
@@ -10,19 +10,43 @@ class Source
   property :title, String
   property :author, String
   property :url, String
+  property :note, String
   property :created_at, DateTime
 end
 
-# automatically create the post table
-Source.auto_upgrade!
+DataMapper.finalize
+
+# automatically create the sources table
+DataMapper.auto_upgrade!
+
 
 # set haml options
 configure do
   set :haml, :format => :html5
 end
 
+post '/new' do
+  # create makes the resource immediately
+  @source = Source.create :title => params[:title],
+    :author => params[:author],
+    :url => params[:url],
+    :note => params[:note],
+    :created_at => Time.now
+
+  redirect to("/"), 303
+end
+
 get '/' do
+  @sources = Source.all :order => [ (:"created_at").desc ]
   haml :index
+end
+
+# delete a resource
+get "/:id/delete" do|id|
+  s = Source.get(id)
+  
+  s.destroy
+  redirect to('/'), 303
 end
 
 # use scss
